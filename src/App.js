2c8f6b69c38822522834b0880c88f564f7c77cfc
manojbuddhiraju <76197954+manojbuddhiraju@users.noreply.github.com>
2023-06-11 21:56:00 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p></p>
+      <p>I am creating blockchain app</p>
     </div>
   );
 }
--- src/components/farmerdetails/farmerdetails.js
+++ src/components/farmerdetails/farmerdetails.js
@@ -3,9 +3,7 @@ import react from "react"
 function farmerdetails() {
     return (
       <div >
-        <Dialog>
-            <DialogTitle>Payment To Farmer</DialogTitle>
-        </Dialog>
+        
       </div>
     );
   }
