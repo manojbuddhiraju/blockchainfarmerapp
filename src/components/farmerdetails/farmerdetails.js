51232d51e1b1b04ac946327d6bd614f5f4cb9c98
Manoj Kumar <manojkumar.buddhiraju417@gmail.com>
2023-06-11 23:36:14 +0530

    Added js files for farmer details and transaction

--- src/components/farmerdetails/farmerdetails.js
+++ src/components/farmerdetails/farmerdetails.js
@@ -1,11 +1,19 @@
-import react from "react"
+import React, { useEffect, useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
 
-function farmerdetails() {
-    return (
-      <div >
-        
-      </div>
-    );
-  }
-  
-  export default farmerdetails;
+function FarmerDetails() {
+
+  return (
+    <>
+ 
+        <Dialog>
+            <DialogTitle>Farmer Details</DialogTitle>
+        </Dialog>
+        <p>Farmer component</p>
+    
+    </>
+  );
+}
+
+export default FarmerDetails;
---
+++ src/components/viewdetails/viewdetails.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+
+function ViewFarmerDetails() {
+
+  return (
+    <>
+      <p>View Farmer Component</p>
+    </>
+  );
+}
+
+export default ViewFarmerDetails;
