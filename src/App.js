3408a69767935b2f559dd4a118550436084ac537
manojbuddhiraju <76197954+manojbuddhiraju@users.noreply.github.com>
2023-06-11 18:49:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p
+        >
+          Application
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
